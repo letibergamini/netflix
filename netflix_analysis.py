a065b7170a7dcf5cff247abd445ca972a76b27d1
letibergamini <leti.bergamini@gmail.com>
2025-06-12 22:45:07 +0200

    Update netflix_analysis.py

--- netflix_analysis.py
+++ netflix_analysis.py
@@ -13,16 +13,18 @@ print(movies.info())
 #DATA CLEANING: adjust the date_added column and create two others more, year and month
 movies["date_added"] = movies["date_added"].str.strip()
 movies["date_added"] = pd.to_datetime(movies["date_added"])
-movies["year_added"] = movies["date_added"].dt.year   # year into year_added
-movies["month_added"] = movies["date_added"].dt.month  # month into month_added
+movies["year_added"] = movies["date_added"].dt.year 
+movies["month_added"] = movies["date_added"].dt.month 
 print(movies[["date_added", "year_added", "month_added"]].head())
 
 #DATA CLEANING: adjust the duration column for TV Series and Movies
 movies["duration_minutes"] = movies["duration"].apply(lambda x: int(x.split(" ")[0]) if "min" in x else None)
 movies["seasons"] = movies["duration"].apply(lambda x: int(x.split(" ")[0]) if "min" not in x else None)
-movies = movies.drop(columns=['duration'])
+movies = movies.drop(columns=["duration"])
 print(movies.head())
 print(movies.tail())
+
+print(movies.groupby("type").size())
 
 #DATA CLEANING: relevant presence of null values
 print(f"Null values for each variable:\n {movies.isnull().sum()}")
@@ -32,6 +34,19 @@ for column in movie_variables:
     null_values = ((movies[column].isnull().sum()/movies.shape[0]) * 100).round(2)
     print(f"Share of null values for variable {column} on total sample: {null_values}")
 
+dir_null = (movies[movies["director"].isnull()][["title", "type", "country"]])
+print(dir_null.groupby("type").size())
+ 
+cast_null = (movies[movies["cast"].isnull()][["title", "type", "country", "release_year"]])
+cast_null_movies = cast_null[cast_null["type"] == "Movie"]
+print(cast_null_movies)
+print(cast_null.groupby("type").size())
+
+movies["decade"] = (movies["release_year"] // 10) * 10
+print(movies[["decade", "release_year"]])
+decades_by_type = movies.groupby(["decade", "type"]).size()
+decade_pivot = decades_by_type.unstack(fill_value=0)
+print(decade_pivot)
 
 #Analysis of the Type of observation (Movie - TV Serie) through graphs
 print(f"Null values in types column:\n {movies["type"].isnull().sum()}")
@@ -41,7 +56,7 @@ type_share = (type_counts/(movies.shape[0])*100).round(2)
 print(type_share)
 print(list(type_counts.index))
 
-plt.pie(type_counts, labels = type_counts.index, autopct="%1.1f%%", startangle=90, colors=["skyblue", "lightcoral"])
+plt.pie(type_counts, labels = type_counts.index, autopct="%1.1f%%", startangle=90, colors=["#FFB6A3", "#FFF1A6"])
 plt.title("Distribution of Movies and TV Shows")
 plt.axis("equal")
 plt.show()
@@ -52,15 +67,14 @@ plt.xlabel("Type of Show")
 plt.ylabel("Frequency")
 plt.show()
 
-
 #Trend over the years of movies and tv series added on the platform (cumulative)
 movies = movies.sort_values(by=["year_added", "month_added"], ascending=True)
 print(movies.head())
 print(movies[["year_added", "month_added"]].tail())
 
 movies_clean = movies.dropna(subset=["year_added", "month_added"])
-movies_clean["year_added"] = movies_clean["year_added"].astype(int) #TO AVOID 2010.0
-movies_clean["month_added"] = movies_clean["month_added"].astype(int) #TO AVOID 11.0
+movies_clean["year_added"] = movies_clean["year_added"].astype(int) 
+movies_clean["month_added"] = movies_clean["month_added"].astype(int)
 print(f"Null values: {movies_clean[["year_added", "month_added"]].isnull().sum()}")
 type_added_by_year = movies_clean.groupby(["year_added", "type"]).size().unstack(fill_value=0)
 print(type_added_by_year)
@@ -68,13 +82,13 @@ print(type_added_by_year)
 cumulative_counts = type_added_by_year.cumsum()
 print(cumulative_counts)
 
-cumulative_counts.plot(kind="line", marker="o", figsize=(10, 6), color = ["skyblue", "pink"]) #use .plot directly on the dataframe, cumulative_counts is a dataframe
+cumulative_counts.plot(kind="line", marker="o", figsize=(10, 6), color = ["skyblue", "pink"])
 plt.title("Cumulative Number of Movies and TV Shows Added Over Years")
 plt.xlabel("Year Added")
 plt.ylabel("Cumulative Count")
 plt.grid(True)
 plt.legend(title="Content Type")
-plt.tight_layout() # it adjusts the spacing between subplots in a figure so that labels, titles, and tick labels don't overlap or get cut off
+plt.tight_layout()
 plt.show()
 
 #Year and month (separately) with the most movies and tv series added
@@ -102,10 +116,9 @@ plt.legend(title="Type", bbox_to_anchor=(1.05, 1), loc="upper left")
 plt.tight_layout()
 plt.show()
 
-
 #Duration and seasons, distribution for movies and tv series
-print(movies[movies["type"] == "Movie"]["duration_minutes"].isnull().sum()) #right order, to look for null values 
-movies_duration_df = movies[movies["type"] == "Movie"][["type", "duration_minutes"]] #filter only movies, than take only type and duration columns
+print(movies[movies["type"] == "Movie"]["duration_minutes"].isnull().sum()) 
+movies_duration_df = movies[movies["type"] == "Movie"][["type", "duration_minutes"]]
 print(movies_duration_df["type"].unique()) #check if filter worked
 print(movies_duration_df.head())
 plt.figure(figsize=(10, 6))
@@ -156,11 +169,44 @@ tvshows_df = movies[movies["type"] == "TV Show"]
 oldest_tvshows = tvshows_df.sort_values(["release_year", "title"])
 print(f"Oldest TV Shows on Netflix:\n {oldest_tvshows[["show_id", "title", "type","release_year"]][:15]}")
 
+
+plt.figure(figsize=(12, 6))
+sns.histplot(data=movies, x="decade", hue="type", binwidth=10, shrink=0.8)
+plt.title("Number of Movies and TV Shows by Decade")
+plt.xlabel("Decade")
+plt.ylabel("Count")
+plt.xticks(rotation=45)
+plt.grid(axis="y", linestyle="--", alpha=0.5)
+plt.tight_layout()
+plt.show()
+
 #Most popular directors on Netflix
 movies_director_clean = movies.dropna(subset = "director")
 print(movies_director_clean["director"].isnull().sum())
+movies_director_clean["director_clean"] = movies_director_clean["director"].str.split(r"\s*,\s*") 
+print(movies_director_clean[["director", "director_clean"]][:20])
 
-most_popular_directors = movies_director_clean["director"].value_counts()[:10]
+directors_flat = movies_director_clean["director_clean"].explode()
+directors_count = directors_flat.value_counts()
+top_directors = directors_count[:20]
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+plt.figure(figsize=(18, 10)) 
+top_directors.plot(kind="bar", color = "#1f77b4")
+plt.title("Top 20 Directors")
+plt.xlabel("Director")
+plt.ylabel("Count")
+plt.tight_layout()
+plt.xticks(rotation=60, ha = "right")
+ax = plt.gca()
+ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+for index, value in enumerate(top_directors):
+    plt.text(index, value, str(value), ha = "center", va = "bottom", fontsize = 8)
+plt.show()
+
+most_popular_directors = movies_director_clean["director_clean"].value_counts()[:10]
 print(f"10 Most popular directors on Netflix (2019):\n {most_popular_directors}")
 most_popular_directors = most_popular_directors.astype(int) #did not work
 
@@ -181,7 +227,7 @@ print(f"Null values in cast: \n {movies["cast"].isnull().sum()}")
 movies_actors_clean = movies.dropna(subset = "cast")
 print(f"Null values in cast: \n {movies_actors_clean["cast"].isnull().sum()}")
 
-movies_actors_clean["cast_clean"] = movies_actors_clean["cast"].str.split(r'\s*,\s*')
+movies_actors_clean["cast_clean"] = movies_actors_clean["cast"].str.split(r"\s*,\s*")
 print(movies_actors_clean[["cast", "cast_clean"]][:20])
 
 actors_flat = movies_actors_clean["cast_clean"].explode()
@@ -189,18 +235,20 @@ actor_counts = actors_flat.value_counts()
 top_actors = actor_counts[:20]
 
 plt.figure(figsize=(12, 6)) 
-top_actors.plot(kind="bar", color = "skyblue")
+top_actors.plot(kind="bar", color = "#1f77b4")
 plt.title("Top 20 Actors")
 plt.xlabel("Actor")
 plt.ylabel("Count")
 plt.tight_layout()
 plt.xticks(rotation=60, ha = "right")
+for index, value in enumerate(top_actors):
+    plt.text(index, value, str(value), ha = "center", va = "bottom", fontsize = 8)
 plt.show()
 
 #Most popular countries
 movies_country_clean = movies.dropna(subset = "country")
 print(movies_country_clean["country"].isnull().sum())
-movies_country_clean["countries_separated"] = movies_country_clean["country"].str.split(r'\s*,\s*')
+movies_country_clean["countries_separated"] = movies_country_clean["country"].str.split(r"\s*,\s*")
 print(movies_country_clean[:15])
 
 countries_flat = movies_country_clean["countries_separated"].explode()
@@ -210,7 +258,7 @@ top_countries = country_counts[:20]
 print(top_countries)
 
 plt.figure(figsize=(10, 6))
-top_countries.plot(kind="bar", color='skyblue')
+top_countries.plot(kind="bar", color="skyblue")
 plt.title("Top 20 Countries by Frequency in Movies")
 plt.xlabel("Country")
 plt.ylabel("Frequency")
@@ -220,7 +268,6 @@ for index, value in enumerate(top_countries):
     plt.text(index, value, str(value), ha = "center", va = "bottom")
 plt.show()
 
-
 #Most Popular genres for Movies
 print(f"Null values in genre: \n {movies["listed_in"].isnull().sum()}")
 movies_genre_clean = movies.dropna(subset = "listed_in")
@@ -229,7 +276,7 @@ print(f"Null values in genre: \n {movies_genre_clean["listed_in"].isnull().sum()
 movies_genre_clean_movies = movies_genre_clean[movies_genre_clean["type"] == "Movie"]
 print(movies_genre_clean_movies["type"].unique())
 print(list(movies_genre_clean_movies.columns))
-movies_genre_clean_movies["genres"] = movies_genre_clean_movies["listed_in"].str.split(r'\s*,\s*')
+movies_genre_clean_movies["genres"] = movies_genre_clean_movies["listed_in"].str.split(r"\s*,\s*")
 print(movies_genre_clean_movies[["listed_in", "genres"]][:15])
 genre_flat = movies_genre_clean_movies["genres"].explode()
 genre_counts = genre_flat.value_counts()
@@ -242,13 +289,14 @@ plt.xlabel("Genre")
 plt.ylabel("Count")
 plt.tight_layout()
 plt.xticks(rotation=60, ha = "right")
+for index, value in enumerate(top_movie_genres):
+    plt.text(index, value, str(value), ha = "center", va = "bottom", fontsize = 8)
 plt.show()
 
 #Most Popular genres for TV Shows
-
 tvshows_genre_clean_movies = movies_genre_clean[movies_genre_clean["type"] == "TV Show"]
 print(tvshows_genre_clean_movies["type"].unique())
-tvshows_genre_clean_movies["genres"] = tvshows_genre_clean_movies["listed_in"].str.split(r'\s*,\s*')
+tvshows_genre_clean_movies["genres"] = tvshows_genre_clean_movies["listed_in"].str.split(r"\s*,\s*")
 print(tvshows_genre_clean_movies[["listed_in", "genres"]][:15])
 genre1_flat = tvshows_genre_clean_movies["genres"].explode()
 genre1_counts = genre1_flat.value_counts()
@@ -261,6 +309,8 @@ plt.xlabel("Genre")
 plt.ylabel("Count")
 plt.tight_layout()
 plt.xticks(rotation=60, ha = "right")
+for index, value in enumerate(top_tvshows_genres):
+    plt.text(index, value, str(value), ha = "center", va = "bottom", fontsize = 8)
 plt.show()
 
 #Ratings for movies and tv shows
@@ -280,15 +330,3 @@ plt.legend(["TV Shows", "Movies"])
 plt.tight_layout()
 plt.xticks(rotation = 45)
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
